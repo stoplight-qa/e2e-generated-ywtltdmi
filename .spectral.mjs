a0fc7f3a9fce5e84f3e014b6459f7452a050ab93
qa-wtfyf-2-ocsi@example.com <qa-wtfyf-2-ocsi@example.com>
2023-02-17 20:38:08 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15329.stoplight-dev.com/api/v1/projects/cHJqOjE3Mw/spectral.js?branch_slug=main&token=dd04bb95-86a0-4e71-a295-1a2e8e1d02a9";
+export default { extends: ruleset };
